8e83463ae07aeec6d3c6fe8c6351ad5a73424bdb
Christopher Probst <christopher.probst@hhu.de>
2013-04-05 23:10:32 +0200

    Initial commit

---
+++ add_scala_links.sh
@@ -0,0 +1,31 @@
+# Removing old distribution
+echo "Removing old distribution"
+bash remove_scala_links.sh
+echo "Done"
+
+# Load scala and unpack
+echo "Loading scala distribution..."
+wget http://www.scala-lang.org/downloads/distrib/files/scala-2.10.1.tgz \
+	&& tar -xzf scala-2.10.1.tgz
+
+echo "Done"
+
+echo "Moving scala distribution..."
+
+# Move the scala folder
+sudo mv scala-2.10.1 /usr/share/scala
+
+# Remove old scala package
+rm -r scala-2.10.1.tgz
+
+echo "Done"
+
+echo "Adding symbolic scala links"
+sudo ln -s /usr/share/scala/bin/scala /usr/bin/scala
+sudo ln -s /usr/share/scala/bin/scalac /usr/bin/scalac
+sudo ln -s /usr/share/scala/bin/fsc /usr/bin/fsc
+sudo ln -s /usr/share/scala/bin/sbaz /usr/bin/sbaz
+sudo ln -s /usr/share/scala/bin/sbaz-setup /usr/bin/sbaz-setup
+sudo ln -s /usr/share/scala/bin/scaladoc /usr/bin/scaladoc
+sudo ln -s /usr/share/scala/bin/scalap /usr/bin/scalap
+echo "Done"
---
+++ remove_scala_links.sh
@@ -0,0 +1,1 @@
+sudo rm -rf /usr/share/scala /usr/bin/scala /usr/bin/scalac /usr/bin/fsc /usr/bin/sbaz /usr/bin/sbaz-setup /usr/bin/scaladoc /usr/bin/scalap

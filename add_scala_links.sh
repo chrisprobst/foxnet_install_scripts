8cb24a77655e396144648ea3fdaeb9425021eb67
Christopher Probst <christopher.probst@hhu.de>
2013-04-06 10:43:11 +0200

    Improve scala scripts

--- add_scala_links.sh
+++ add_scala_links.sh
@@ -1,15 +1,13 @@
 # This script installs scala
 
 # Removing old distribution
-echo "Removing old distribution"
 bash remove_scala_links.sh
-echo "Done"
 echo
 echo
 
 # Load scala and unpack
 echo "Loading scala distribution..."
-wget http://www.scala-lang.org/downloads/distrib/files/scala-2.10.1.tgz \
+wget -N http://www.scala-lang.org/downloads/distrib/files/scala-2.10.1.tgz \
 	&& tar -xzf scala-2.10.1.tgz
 echo "Done"
 echo
@@ -49,13 +47,3 @@ do
 	echo 
 done
 
-
-#echo "Adding symbolic scala links"
-#sudo ln -s /usr/share/scala/bin/scala /usr/bin/scala
-#sudo ln -s /usr/share/scala/bin/scalac /usr/bin/scalac
-#sudo ln -s /usr/share/scala/bin/fsc /usr/bin/fsc
-#sudo ln -s /usr/share/scala/bin/sbaz /usr/bin/sbaz
-#sudo ln -s /usr/share/scala/bin/sbaz-setup /usr/bin/sbaz-setup
-#sudo ln -s /usr/share/scala/bin/scaladoc /usr/bin/scaladoc
-#sudo ln -s /usr/share/scala/bin/scalap /usr/bin/scalap
-#echo "Done"
